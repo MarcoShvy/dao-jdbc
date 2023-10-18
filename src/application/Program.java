3218bf2095d1a480392db06305d900f0511229d9
Marco <mass7813@gmail.com>
2023-10-17 22:47:36 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,15 +7,29 @@ import model.entites.Seller;
 
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args){
 
+        Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println("=== TEST 1: seller findById");
+        System.out.println("=== TEST 1: seller findById ===");
         Seller seller = sellerDao.findById(3);
-
         System.out.println(seller);
+        System.out.println("\n=== TEST 2: seller findByDepartment ===");
+        System.out.print("Qual departamento deseja encontrar");
+        int departmentId = sc.nextInt();
+        Department department = new Department(departmentId, null);
+        List<Seller> list = sellerDao.findByDepartment(department);
+        if (list == null) {
+            System.out.println("Não existe departamento com este ID");
+        } else {
+            for (Seller seller1 : list) {
+                System.out.println(seller1);
+            }
+        }
     }
 }
