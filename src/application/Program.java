236871554bebcf2e29e6eed4623dec9c310b6a7d
Marco <mass7813@gmail.com>
2023-10-17 23:23:02 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,6 +15,7 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) throws ParseException {
 
+        SimpleDateFormat sdt = new SimpleDateFormat("dd/MM/yyyy");
         Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -40,23 +41,51 @@ public class Program {
             System.out.println(seller1);
         }
 
-        System.out.println("\n=== TEST 4: seller insert ===");
-        Seller newSeller = new Seller();
-        SimpleDateFormat sdt = new SimpleDateFormat("dd/MM/yyyy");
+//        System.out.println("\n=== TEST 4: seller insert ===");
+//        Seller newSeller = new Seller();
+//        sc.nextLine();
+//        System.out.print("Name: ");
+//        newSeller.setName(sc.nextLine());
+//        System.out.print("Email: ");
+//        newSeller.setEmail(sc.nextLine());
+//        System.out.print("Data nascimento(dd/MM/yyyy): ");
+//        newSeller.setBirthDate(sdt.parse(sc.nextLine()));
+//        System.out.print("Base Salary: ");
+//        newSeller.setBaseSalary(sc.nextDouble());
+//        System.out.print("Department (1-4): ");
+//        newSeller.setDepartment(new Department(sc.nextInt(), null));
+//        sellerDao.insert(newSeller);
+//        System.out.println("Inserted! new Id = "+ newSeller.getId());
 
+        System.out.println("\n=== TEST 5: seller update ===");
+        System.out.printf("Qual id deseja mudar: ");
+        seller = sellerDao.findById(sc.nextInt());
+        System.out.print("Oque deseja mudar 1-Nome, 2-Email, 3-BirthDate, 4-BaseSalary, 5-Department: ");
+        int choose = sc.nextInt();
         sc.nextLine();
-        System.out.print("Name: ");
-        newSeller.setName(sc.nextLine());
-        System.out.print("Email: ");
-        newSeller.setEmail(sc.nextLine());
-        System.out.print("Data nascimento(dd/MM/yyyy): ");
-        newSeller.setBirthDate(sdt.parse(sc.nextLine()));
-        System.out.print("Base Salary: ");
-        newSeller.setBaseSalary(sc.nextDouble());
-        System.out.print("Department (1-4): ");
-        newSeller.setDepartment(new Department(sc.nextInt(), null));
-
-        sellerDao.insert(newSeller);
-        System.out.println("Inserted! new Id = "+ newSeller.getId());
+        switch (choose) {
+            case 1:
+                System.out.printf("Digite o nome: ");
+                seller.setName(sc.nextLine());
+                break;
+            case 2:
+                System.out.printf("Digite o email: ");
+                seller.setEmail(sc.nextLine());
+                break;
+            case 3:
+                System.out.printf("Digite a data nascimento");
+                seller.setBirthDate(sdt.parse(sc.nextLine()));
+                break;
+            case 4:
+                System.out.printf("digite o salário");
+                seller.setBaseSalary(sc.nextDouble());
+                break;
+            case 5:
+                System.out.printf("Digite o departamento");
+                seller.setDepartment(new Department(sc.nextInt(), null));
+                break;
+        }
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
